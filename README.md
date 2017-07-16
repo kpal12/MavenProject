0dab8782de6a7f772d0c93153389a29e8e4f48be
kpal12 <kppalcse89@gmail.com>
2017-07-16 19:03:04 +0530

    commiting new files

---
+++ README.md
@@ -0,0 +1,2 @@
+# hello-world-maven-unit-test
+Hello World Java Application with Maven and Unit Tests
---
+++ src/main/java/com/efsavage/App.java
@@ -0,0 +1,11 @@
+/**
+ * Hello world..This is a feature 2.0
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Welcome to Webster Demo" );
+    }
+} 
